84f85d24aa54c3991c68406fd70ed8afc26c6f73
hooyoungyi <yihooyung@gmail.com>
2024-07-01 12:29:32 +0900

    [Feature-0701] - {ProductController} 상품조회 #1

--- src/main/java/com/clds/bottletalk/product/repository/ProductRepository.java
+++ src/main/java/com/clds/bottletalk/product/repository/ProductRepository.java
@@ -3,5 +3,10 @@ package com.clds.bottletalk.product.repository;
 import com.clds.bottletalk.product.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+
+    List<Product> findByNameContainingIgnoreCase(String search);
 }
